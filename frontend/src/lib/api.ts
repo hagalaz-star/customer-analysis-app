9b15df5b63df2a1830f10193f97b324a15fef56f
Cyber Nova <saei07@naver.com>
2025-09-02 16:48:16 +0900

    Vercel 배포 설정 추가

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { CustomerProfile } from "../types/types";
 
-const API_HOST = process.env.NEXT_PUBLIC_API_HOST || "http://localhost:8000";
+// const API_HOST = process.env.NEXT_PUBLIC_API_HOST || "http://localhost:8000";
 
 export async function analyzeCustomer(formData: CustomerProfile) {
   try {
-    const response = await axios.post(`${API_HOST}/api/analysis`, formData);
+    const response = await axios.post(`/api/analysis`, formData);
 
     return response.data;
   } catch (error) {
